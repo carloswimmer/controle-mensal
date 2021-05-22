2fc48bdb8e5355f96953d02cbfe4a1f3cf7fa124
Carlos Wimmer <coiote777@gmail.com>
2021-05-22 01:11:51 -0300

    Create auth context

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,54 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { User } from '@firebase/auth-types'
+import { auth } from '../firebase'
+
+export interface AuthData {
+  email: string
+  password: string
+}
+
+interface AuthContextData {
+  user: User
+  signUp(data: AuthData): void
+  signIn(data: AuthData): void
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+
+const AuthProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [user, setUser] = useState({} as User)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      user && setUser(user)
+      setLoading(false)
+    })
+    return unsubscribe
+  }, [])
+
+  const signUp = useCallback(({ email, password }: AuthData) => {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }, [])
+
+  const signIn = useCallback(({ email, password }: AuthData) => {
+    return auth.signInWithEmailAndPassword(email, password)
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, signUp, signIn }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
+
+const useAuth = (): AuthContextData => useContext(AuthContext)
+
+export { useAuth, AuthProvider }
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from 'react'
 import { DarkModeProvider } from './darkMode'
 import { ThemeProvider } from './theme'
+import { AuthProvider } from './auth'
 import { ToastProvider } from './toast'
 
 const AppProvider = ({ children }: PropsWithChildren<{}>) => {
   return (
     <DarkModeProvider>
       <ThemeProvider>
-        <ToastProvider>{children}</ToastProvider>
+        <AuthProvider>
+          <ToastProvider>{children}</ToastProvider>
+        </AuthProvider>
       </ThemeProvider>
     </DarkModeProvider>
   )
