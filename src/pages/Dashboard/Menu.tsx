f0647792a1e9815855e3beb14d26300de1b32beb
Carlos Wimmer <coiote777@gmail.com>
2021-05-23 17:12:06 -0300

    Isolate dashboard menu

---
+++ src/pages/Dashboard/Menu.tsx
@@ -0,0 +1,43 @@
+import { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Grid, Button } from '@material-ui/core'
+import { useAuth } from '../../hooks/auth'
+import { useToast } from '../../hooks/toast'
+import { handleError } from '../../components/controls/utils'
+
+const Menu = () => {
+  const { signOut } = useAuth()
+  const { addToast } = useToast()
+  const history = useHistory()
+
+  const handleLogOut = useCallback(async () => {
+    try {
+      await signOut()
+      history.push('/')
+    } catch (error) {
+      const message = handleError(error)
+      addToast({ text: message })
+    }
+  }, [addToast, history, signOut])
+
+  return (
+    <Grid container justify="flex-end" spacing={2}>
+      <Grid item>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => history.push('/update-profile')}
+        >
+          Profile
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button variant="contained" color="primary" onClick={handleLogOut}>
+          Sair
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Menu
